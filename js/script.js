e6959023db5beb99dcb6544f2827bf5d57ad1340
Maria Karowska <mariakarowska@gmail.com>
2024-01-24 17:29:54 +0100

    biding events moved to a seperate function

--- js/script.js
+++ js/script.js
@@ -29,6 +29,25 @@
         render();
     };
 
+    const bindEvents = () => {
+        const removeButtons = document.querySelectorAll(".js-remove");
+
+            removeButtons.forEach((removeButton, index) => {
+                removeButton.addEventListener("click", () => {
+                   removeTask(index);
+                });
+            });
+
+            const toggleDoneButtons = document.querySelectorAll(".js-done");
+
+            toggleDoneButtons.forEach((toggleDoneButton, index) => {
+                toggleDoneButton.addEventListener("click", () => {
+                   toggleTaskDone(index);
+                });
+            });
+
+    };
+
     const render = () => {
         let htmlString = "";
 
@@ -45,22 +64,7 @@
 
             document.querySelector(".js-tasks").innerHTML = htmlString;
 
-            const removeButtons = document.querySelectorAll(".js-remove");
-
-            removeButtons.forEach((removeButton, index) => {
-                removeButton.addEventListener("click", () => {
-                   removeTask(index);
-                })
-            })
-
-            const toggleDoneButtons = document.querySelectorAll(".js-done");
-
-            toggleDoneButtons.forEach((toggleDoneButton, index) => {
-                toggleDoneButton.addEventListener("click", () => {
-                   toggleTaskDone(index);
-                })
-            })
-
+            bindEvents();
     };
 
     const onFormSubmit = (event) => {
