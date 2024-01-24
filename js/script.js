413ddd565eaeeb139a91f7913aec4d8d8d5a4a6d
Maria Karowska <mariakarowska@gmail.com>
2024-01-24 17:00:53 +0100

    remove tasks

--- js/script.js
+++ js/script.js
@@ -11,45 +11,60 @@
 
     ];
 
-    const render = () => {
-        let htmlString = "";
-
-        for (const task of tasks) {
-            htmlString += `
-            <li 
-            class="list_item ${task.done ? " list_item--done" : ""}">
-            ${task.content}
-            </li>
-            `;
-        }
-
-            document.querySelector(".js-tasks").innerHTML = htmlString;
-    };
-
     const addNewTask = (newTaskContent) => {
-        task.push({
+        tasks.push({
             content: newTaskContent,
         });
 
         render();
     };
 
+    const removeTask = (index) => {
+        tasks.splice(index,1);
+        render();
+    };
+
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+            <li 
+            class="list_item ${task.done ? "list__item--done" : ""}">
+            <button class="js-remove">Usuń</button>
+            ${task.content}
+            </li>
+            `;
+        }
+
+            document.querySelector(".js-tasks").innerHTML = htmlString;
+
+            const removeButtons = document.querySelectorAll(".js-remove");
+
+            removeButtons.forEach((removeButton, index) => {
+                removeButton.addEventListener("click", () => {
+                   removeTask(index);
+                })
+            })
+    };
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+        if (newTaskContent === "") {
+            return;
+        }
+        addNewTask(newTaskContent);
+    };
+    
     const init = () => {
         render();
 
         const form = document.querySelector(".js-form");
 
-        form.addEventListener("submit", (event)) => {
-            event.preventDefault();
-
-            const newTaskContent = document.querySelector(".js-newTask").value.trim();
-
-            if (newTaskContent === "") {
-                return;
-            }
-            addNewTask(newTaskContent);
-
-        });
+        form.addEventListener("submit", onFormSubmit);
     };
 
     init();
